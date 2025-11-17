64eeb351e66a50d0709a731f78651b24802901f2
YYchainsAw <2560634677@qq.com>
2025-11-17 16:47:03 +0800

    注册接口参数校验

--- big-event/src/main/java/com/yychainsaw/controller/UserController.java
+++ big-event/src/main/java/com/yychainsaw/controller/UserController.java
@@ -3,20 +3,23 @@ package com.yychainsaw.controller;
 import com.yychainsaw.pojo.Result;
 import com.yychainsaw.pojo.User;
 import com.yychainsaw.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public Result register(String username, String password) {
+    public Result register(@Pattern(regexp = "^\\S{5,16}$") String username,@Pattern(regexp = "^\\S{5,16}$") String password) {
         //查询数据库是否存在该用户
         User u = userService.findByUsername(username);
 
