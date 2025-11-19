ebf1ac4bea99fa1f966aef1cf4230e8970bcd661
YYchainsAw <2560634677@qq.com>
2025-11-19 21:36:17 +0800

    文件上传本地

---
+++ big-event/src/main/java/com/yychainsaw/controller/FileUploadController.java
@@ -0,0 +1,25 @@
+package com.yychainsaw.controller;
+
+import com.yychainsaw.pojo.Result;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+public class FileUploadController {
+    
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file) throws IOException {
+        String originalFilename = file.getOriginalFilename();
+
+        //保证文件名唯一，可以使用UUID或者时间戳等方式
+        String filename = UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf("."));
+
+        file.transferTo(new File("C:\\Users\\YYchainsaw\\IdeaProjects\\files\\" + filename));
+        return Result.success("url地址");
+    }
+}
